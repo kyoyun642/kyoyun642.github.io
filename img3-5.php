f5c15a7ad10e2e623ec0b102b8e3ca052798296a
kyoyun642 <rydbs12345678@gmail.com>
2025-04-10 07:25:11 +0900

    Update img3-5.php

--- img3-5.php
+++ img3-5.php
@@ -1,45 +1,55 @@
-<?php
-$year = date("Y");
-$month = date("n");
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP 달력</title>
+</head>
+<body>
+  <h1>달력 출력 예제</h1>
 
-$first_day = mktime(0, 0, 0, $month, 1, $year);
-$first_day_weekday = date("w", $first_day);
-$total_days = date("t", $first_day);
-$today = date("j");
+  <table border="1">
+    <tr>
+      <th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>
+    </tr>
+    <tr>
+    <?php
+    $year = date("Y");
+    $month = date("n");
+    $today = date("j");
+    $first_day = mktime(0, 0, 0, $month, 1, $year);
+    $first_day_weekday = date("w", $first_day);
+    $total_days = date("t", $first_day);
 
-echo "<h2>{$year}년 {$month}월</h2>";
-echo "<table border='1'>";
-echo "<tr>
-  <th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>
-</tr><tr>";
+    for ($i = 0; $i < $first_day_weekday; $i++) {
+        echo "<td></td>";
+    }
 
-for ($i = 0; $i < $first_day_weekday; $i++) {
-  echo "<td></td>";
-}
+    $week_day = $first_day_weekday;
 
-$week_day = $first_day_weekday;
+    for ($day = 1; $day <= $total_days; $day++) {
+        if ($day == $today) {
+            echo "<td><b>{$day}</b></td>";
+        } else {
+            echo "<td>{$day}</td>";
+        }
 
-for ($day = 1; $day <= $total_days; $day++) {
-  if ($day == $today) {
-    echo "<td><b>$day</b></td>";
-  } else {
-    echo "<td>$day</td>";
-  }
+        $week_day++;
+        if ($week_day == 7) {
+            echo "</tr><tr>";
+            $week_day = 0;
+        }
+    }
 
-  $week_day++;
-  if ($week_day == 7) {
-    echo "</tr><tr>";
-    $week_day = 0;
-  }
-}
+    if ($week_day != 0) {
+        for ($i = $week_day; $i < 7; $i++) {
+            echo "<td></td>";
+        }
+    }
+    ?>
+    </tr>
+  </table>
 
-if ($week_day != 0) {
-  for ($i = $week_day; $i < 7; $i++) {
-    echo "<td></td>";
-  }
-}
-
-echo "</tr></table>";
-?>
+</body>
+</html>
 
 
